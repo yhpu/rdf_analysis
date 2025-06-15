2966150977c15f6699f7de567ffb7c61a543bf8e
yhpu <puyahong@dp.tech>
2025-06-15 12:44:47 +0800

    更新.gitignore，清理无用文件

--- rdf/__init__.py
+++
@@ -1,5 +0,0 @@
-from .vasp_rdf import process as process_vasp
-from .lmp_rdf import process as process_lmp
-from .gmx_rdf import process as process_gmx
-
-__all__ = ['vasp_rdf', 'lmp_rdf', 'gmx_rdf']
